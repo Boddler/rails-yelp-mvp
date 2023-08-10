6c2645112222a744ac6a0ce9edefca9746005baa
Barry Roberts <barryjroberts@gmail.com>
2023-08-10 16:50:26 +0900

    updating...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+categories = %w[chinese italian japanese french belgian]
+
+15.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  category = categories.sample
+  phone_number = Faker::PhoneNumber.phone_number
+
+  Restaurant.create(
+    name: name,
+    address: address,
+    category: category,
+    phone_number: phone_number
+  )
+end
